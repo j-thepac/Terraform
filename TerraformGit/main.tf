4bccfdcd04d1f7217ad51adb83482e24581a1792
Deepak <j.thepac@gmail.com>
2024-12-18 13:26:52 +0530

    working code

---
+++ TerraformGit/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "key" {
+  type      = string
+  sensitive = true
+}
+
+provider "github" {
+  token = var.key
+  owner = "j.thepac@gmail.com"
+}
+
+resource "github_repository" "example" {
+  name        = "my-new-repo"
+  description = "This is my new GitHub repository created using Terraform"
+  visibility  = "public"
+}
