63fc20545dc60dd00873f77f819aeceda56d05dc
Deepak <j.thepac@gmail.com>
2024-12-22 15:33:04 +0530

    Update main.tf

--- TerraformTest/main.tf
+++ TerraformTest/main.tf
@@ -1,1 +1,22 @@
 #Hi
+
+terraform {
+    required_providers {
+        local = {
+            source = "hashicorp/local"
+        }
+    }
+}
+
+resource "local_file" "hello" {
+    content  = "hello world"
+    filename = "1.txt"
+}
+
+output "file_content" {
+    value = local_file.1.content
+}
+
+output "file_name" {
+    value = local_file.1.filename
+}
