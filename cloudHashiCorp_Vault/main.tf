f32c62b4f609e701e9f9f5980971f46fc9b1ea60
Deepak <j.thepac@gmail.com>
2025-01-05 15:00:02 +0530

    .github/

--- cloudHashiCorp_Vault/main.tf
+++ cloudHashiCorp_Vault/main.tf
@@ -2,21 +2,19 @@
 
 - Start Dockerplayground
 - docker run -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=dev-only-token' hashicorp/vault
-- cli- vault read cubbyhole/test
+(Save above in sh file and run sh file.sh)
+- Open port and login with "dev-only-token"
+- address:  Vault > Tools > APi Explorer
+- token : dev-only-token
+
 */
 
-
-variable "HASHICORPTOKEN" {
-  type      = string
-  sensitive = true
-}
-
 provider "vault" {
-  address = "http://ip172-18-0-34-ctt2j48l2o9000dpetqg-8200.direct.labs.play-with-docker.com/"
+  address = "http://ip172-18-0-31-ctt4rmqim2rg00aoo240-8200.direct.labs.play-with-docker.com/"
   skip_tls_verify = true
-  token = var.HASHICORPTOKEN
+  token = "dev-only-token"
 }
 
 data "vault_generic_secret" "my_secret" {
-  path = "myPath/dict"  # Correct path
+  path = "secrets/test"  
 }
